be1e16eb0f742f9ee3d97c394f806434757dd5ff
nillinav <157716789+nillinav@users.noreply.github.com>
2024-01-28 07:31:53 +0000

    Replace DB with dict

--- GemPlanner/Planner/views.py
+++ GemPlanner/Planner/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from .models import *
+from .funcn import *
 # Create your views here.
 
 def home(request):
     return render(request,'Planner/home.html',{})
 
 def gchat(request):
-    return render(request,'Planner/my_gemplanner.html',{})
+    
+    
+    return render(request,'Planner/my_gemplanner.html',{'gemPlan1':gemPlan1})
+
 
 def gemplans(request):
-    return render(request, 'Planner/gemplanList.html',{})
+    return render(request, 'Planner/gemplanList.html',{'gemPlan1':gemPlan1})
