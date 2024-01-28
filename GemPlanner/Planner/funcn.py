b230db64e100db447e1beb949c3e41f9fbb8c21d
nillinav <157716789+nillinav@users.noreply.github.com>
2024-01-28 07:15:06 +0000

    Added various fns

--- GemPlanner/Planner/funcn.py
+++ GemPlanner/Planner/funcn.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 
 from IPython.display import display
 from IPython.display import Markdown
-from Planner.models import *
+#from Planner.models import *
 import requests
 from vertexai.preview.generative_models import (
     Content,
@@ -17,7 +17,7 @@ from vertexai.preview.generative_models import (
     Tool,
 )
 
-gemPlan1=GemPlans(name="GemPlans")
+gemPlan1=dict()
 
 GOOGLE_API_KEY='AIzaSyAlbo3wlPw0oltmVpM9dEjU-3JFLXZfwnI'
 
@@ -35,11 +35,15 @@ model = genai.GenerativeModel('gemini-pro')
 
 def identifyGemplan(prompt):
     GQ=f'''
-Based on the below prompt, identify which gemplan(aka task or event) is being mentioned:-
+Based on the below prompt, identify which gemplan(aka task or event) from the given dictionary:-
 {prompt}
+
+The dictionary is {gemPlan1}
 '''
     task=model.generate_content(GQ).text
-    return task
+    for i in gemPlan1:
+        if i in task:
+            return i
 
 def convertDateTime(x):
     time=datetime.datetime.strptime(x,'%d %b %Y %I:%M%p')
@@ -55,22 +59,46 @@ In the below prompt, extract the task/event, date and time and when to be remind
 
     details=list(Dict.values())
     
-    gemPlan1.create(name=details[0],date=convertDateTime(details[1]),is_completed=False)
-    gemPlan1.save()
+    '''gemPlan1.create(name=details[0],date=convertDateTime(details[1]),is_completed=False)
+    gemPlan1.save()'''
+
+    gemPlan1[details[0]]=[convertDateTime(details[1]),False]
 
 
 def editTask(prompt):
+    reT=identifyGemplan(prompt)
     Try=f'''
-For the given below prompt, Identify the task mentioned (or gemplan. Assume gemplan is another term for task or event) and identify the changes needed to be made to a dictionary in python:-
-"{prompt}"
+For the given below prompt, and identify the changes in time required to be made.:-
+"{prompt}. The identified task is {reT}"
     '''
     response=model.generate_content(Try)
-    Dict=eval(response.text[(response.text).find('{'):(response.text).find('}')+1])
+    newTime=response.text
 
-    details=list(Dict.values())
+    #details=list(Dict.values())
     
-    gemPlan1.create(name=details[0],date=convertDateTime(details[1]),is_completed=False)
-    gemPlan1.save()
+    gemPlan1[reT]=[convertDateTime(newTime,False)]
 
-def removeTask(prompt):
-    ...
+def removeGemplan(prompt):
+    task = identifyGemplan(prompt)
+    del gemPlan1[task]
+
+def markGemplan(prompt):
+    task = identifyGemplan(prompt)
+    gemPlan1[task][1] = True
+'''
+def viewAll():
+    for i in gemPlan1:
+        print(f'Gemplan: {i} Date: {gemPlan1[i][0]} Completed: {gemPlan1[i][0]}')
+
+def viewCompleted():
+    for i in gemPlan1:
+        if gemPlan1[i][1]:
+            print(f'Gemplan: {i} Date: {gemPlan1[i][0]} Completed: {gemPlan1[i][0]}')
+
+def viewPending():
+    for i in gemPlan1:
+        if not gemPlan1[i][1]:
+            print(f'Gemplan: {i} Date: {gemPlan1[i][0]} Completed: {gemPlan1[i][0]}')
+'''
+def generalQuery(prompt):
+    model.generate_content(prompt)
