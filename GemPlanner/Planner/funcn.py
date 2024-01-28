13d2e9c5f600c206c38cdea7db83a385a1e04567
Koala-123 <dhirajdinesh@outlook.com>
2024-01-28 11:57:11 +0530

    huikbs

--- GemPlanner/Planner/funcn.py
+++ GemPlanner/Planner/funcn.py
@@ -24,14 +24,22 @@ GOOGLE_API_KEY='AIzaSyAlbo3wlPw0oltmVpM9dEjU-3JFLXZfwnI'
 model = genai.GenerativeModel('gemini-pro')
 
 
-identifyGemplan=FunctionDeclaration(
+'''identifyGemplan=FunctionDeclaration(
     name="Identify Task",
     description="Identifies the task from the database",
     parameters={
         "type":"object",
 
     }
-)
+)'''
+
+def identifyGemplan(prompt):
+    GQ=f'''
+Based on the below prompt, identify which gemplan(aka task or event) is being mentioned:-
+{prompt}
+'''
+    task=model.generate_content(GQ).text
+    return task
 
 def convertDateTime(x):
     time=datetime.datetime.strptime(x,'%d %b %Y %I:%M%p')
@@ -45,9 +53,7 @@ In the below prompt, extract the task/event, date and time and when to be remind
     response=model.generate_content(Try)
     Dict=eval(response.text[(response.text).find('{'):(response.text).find('}')+1])
 
-    details=list()
-    for i in Dict:
-        details.append(Dict[i])
+    details=list(Dict.values())
     
     gemPlan1.create(name=details[0],date=convertDateTime(details[1]),is_completed=False)
     gemPlan1.save()
